038b9a255f12544872d822091ce57d0a8ff23691
kazuki ikuta <43980494+kazukikuu@users.noreply.github.com>
2019-09-15 15:09:47 +0900

    Add files via upload

---
+++ express/app.rb
@@ -0,0 +1,48 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require 'open-uri'
+require 'json'
+require 'net/http'
+
+require 'sinatra/activerecord'
+require './models'
+
+get '/' do
+  @histories = History.all
+  erb :form
+end
+
+get '/' do
+  erb :form
+end
+
+get '/list' do
+  History.create!(x: params[:x],y: params[:y])
+  uri = URI("http://express.heartrails.com/api/json")
+  uri.query = URI.encode_www_form({
+   method: "getStations",
+   x: params[:x],
+   y: params[:y]
+  })
+  res = Net::HTTP.get_response(uri)
+  json = JSON.parse(res.body)
+  @stations = json["response"]["station"]
+  erb :list
+end
+
+get '/api/station' do
+  uri = URI("http://express.heartrails.com/api/json")
+  uri.query = URI.encode_www_form({
+   method: "getStations",
+   line: params[:line],
+   name: params[:name]
+   })
+   res = Net::HTTP.get_response(uri)
+  json = JSON.parse(res.body)
+  response = {
+    next: json["response"]["station"][0]["next"]
+  }
+  json response
+
+end
