fd37f5fe51d07098a922dba77fc4ac9b59ea1ea3
kazuki ikuta <43980494+kazukikuu@users.noreply.github.com>
2019-09-26 22:01:49 +0900

    Add files via upload

--- question_form_temp5/db/migrate/20190925071540_create_answers.rb
+++ question_form_temp5/db/migrate/20190925071540_create_answers.rb
@@ -1,14 +1,14 @@
- class CreateAnswers < ActiveRecord::Migration[5.2]
+class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
-    create_table :answers do |t|
-    t.string :name
-    t.integer :age
-    t.string :grade
-    t.string :gender
-    t.string :email
-    t.string :date
-    t.string :password
-    t.text :content
+    create_table :answers do |t|
+    t.string :name
+    t.integer :age
+    t.string :grade
+    t.string :gender
+    t.string :email
+    t.string :date
+    t.string :password
+    t.text :content
 
     t.timestamps null: false
    end
