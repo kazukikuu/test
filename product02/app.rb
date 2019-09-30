abe0e880baaa08343283a2e768f78447ea49840a
kazuki ikuta <43980494+kazukikuu@users.noreply.github.com>
2019-09-30 13:41:07 +0900

    Add files via upload

--- product02/app.rb
+++ product02/app.rb
@@ -23,16 +23,19 @@ before '/tasks' do
 end
 
 get '/' do
-   uri = URI(“https://itunes.apple.com/search”)
-    uri.query = URI.encode_www_form ({term:keyword,country:"US",media:"music",limit:10
-   })
-  res = Net::HTTP.get_response(uri)
-  returned_json = JSON.parse(res.body)
-  json = JSON.parse(res.body)
-  @music =  returned_json["results"]
-erb :index
+  erb :form
 end
 
+get '/list' do
+   keyword = params[:keyword]
+   uri = URI("https://itunes.apple.com/search")
+   uri.query = URI.encode_www_form({term: keyword,country: "JP", media: "music", limit: 10 })
+   res = Net::HTTP.get_response(uri)
+   returned_json = JSON.parse(res.body)
+   json = JSON.parse(res.body)
+   @music = returned_json["results"]
+ erb :list
+end
 
 
 
